db168ae416c96cb779159bfc2942f25328f1043a
LexieVaneska <125613116+LexieVaneska@users.noreply.github.com>
2026-05-07 11:37:31 -0600

    fix ruta

--- htdocs/public/index.php
+++ htdocs/public/index.php
@@ -179,7 +179,7 @@
                  Impacto: Visual, no funcional
             -->
             <a href="/" style="padding: 5px 10px; display: flex; align-items: center;">
-                <img src="/img/TEC.png" alt="TEC - Tecnológico de Costa Rica" style="height: 40px; margin-right: 10px;">
+                <img src="/images/TEC.png" alt="TEC - Tecnológico de Costa Rica" style="height: 40px; margin-right: 10px;">
             </a>
             <a  style="color: white; padding: 7px 10px 8px 10px;"
                 href=""
